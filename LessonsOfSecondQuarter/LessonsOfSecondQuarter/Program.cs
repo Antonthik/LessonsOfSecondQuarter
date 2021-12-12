f966a4629aa927e2ad9ae974bb061f320f305dc9
Antonthik <93084395+Antonthik@users.noreply.github.com>
2021-12-13 00:20:43 +0300

    Add files via upload

---
+++ LessonsOfSecondQuarter/LessonsOfSecondQuarter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LessonsOfSecondQuarter
+{
+    class Program
+    {
+        static List<Interflesson> lessons = new List<Interflesson>()
+        {
+            new  Lesson1_task1(),
+            new  Lesson1_task2(),
+            new  Lesson1_task3(),
+            new  Lesson2_task1()
+        };
+        static void Main(string[] args)
+        {
+
+            //Lesson1_task1();
+            Console.WriteLine($"Список доступных уроков:");
+            foreach (Interflesson lesson in lessons)
+            {
+                Console.WriteLine($"имя: {lesson.NameLesson} описание:{lesson.Discription}");
+            }
+
+            Console.WriteLine($"Введите имя урока для запуска:");
+            string input = Console.ReadLine();
+
+            foreach (Interflesson lesson in lessons)
+            {
+                if (input == lesson.NameLesson)
+                {
+                    lesson.Demo();
+                }
+                
+
+            }
+
+
+    
+        }
+
+
+        
+        
+
+    }
+}
